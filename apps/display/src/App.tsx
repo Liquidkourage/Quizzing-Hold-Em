7f89423b8dadd2437c574bb7f1867edd557d9b84
liquidkourage <liquidkouragekaraoke@gmail.com>
2025-08-21 10:24:08 -0500

    Fine-tune static community cards positioning - moved down and right by 8px to align with animated cards

--- apps/display/src/App.tsx
+++ apps/display/src/App.tsx
@@ -954,8 +954,8 @@ function DisplayApp() {
                         key={i}
                         className="absolute"
                         style={{
-                          left: cardX - (64 * 1.5 / 2), // Offset by half the scaled card width
-                          top: cardY - (96 * 1.5 / 2), // Offset by half the scaled card height
+                          left: cardX - (64 * 1.5 / 2) + 8, // Offset by half the scaled card width + small right offset
+                          top: cardY - (96 * 1.5 / 2) + 8, // Offset by half the scaled card height + small down offset
                           transform: 'scale(1.5)', // Scale to match animation
                           transformOrigin: '0 0' // Scale from top-left corner
                         }}
